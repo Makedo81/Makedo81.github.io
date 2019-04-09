b767d633370d5470b461d7c1d6503c348c450c2e
Makedo81 <domi.stan@wp.pl>
2019-04-09 19:10:33 +0200

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://hidden-hollows-87876.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://hidden-hollows-87876.herokuapp.com/v1/trello/';
+  const apiRoot = '//hidden-hollows-87876.herokuapp.com/v1/task/';
+  const trelloApiRoot = '//hidden-hollows-87876.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
